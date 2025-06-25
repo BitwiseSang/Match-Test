8ed821641621d80c816919d1b88971d7ecf511b0
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-06-25 20:08:32 +0300

    fix: add upload middleware to auth route
    Added a middleware to the signup route which handles file uploads. In the sign up context, it handles the user avatar/profile.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 import { signup, login } from '../controllers/authController.js';
+import { single } from '../middleware/upload.js';
 
 // Signup route with avatar upload
-router.post('/signup', signup);
+router.post('/signup', single('profileAvatar'), signup);
 
 // Login route
 router.post('/login', login);
